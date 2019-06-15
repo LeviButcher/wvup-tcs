c59e5b64c54970787ce5345576ec6096120c33cc
Levi Butcher <lbutche3@wvup.edu>
2019-06-15 12:54:10 -0400

    Theme support added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Router } from '@reach/router';
+import { ThemeProvider } from 'styled-components';
 import { KioskLayout, Home, SignIn } from './components/kiosk';
 import { DashboardLayout } from './components/dashboard';
+import Theme from './theme.json';
 
 function App() {
   return (
     <div>
-      <Router>
-        <KioskLayout path="/">
-          <Home path="/" />
-          <SignIn path="/signin" />
-        </KioskLayout>
-        <DashboardLayout path="/dashboard">
-          <Hello path="/" />
-        </DashboardLayout>
-      </Router>
+      <ThemeProvider theme={Theme}>
+        <Router>
+          <KioskLayout path="/">
+            <Home path="/" />
+            <SignIn path="/signin" />
+          </KioskLayout>
+          <DashboardLayout path="/dashboard">
+            <Hello path="/" />
+          </DashboardLayout>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
