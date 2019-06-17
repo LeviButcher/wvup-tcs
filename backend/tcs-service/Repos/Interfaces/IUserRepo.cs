a0b66f7af296e2c5731c09bd393dce587a07f7e4
Aaron <a.rcottrill521@gmail.com>
2019-06-17 19:59:04 -0400

    Try to add authentication
    Unfortunately I cannot create the migration necessary to add the user table nor try it myself. I was inspired by code provided by Jason Wattmore at https://jasonwatmore.com/post/2018/08/14/aspnet-core-21-jwt-authentication-tutorial-with-example-api#user-service-cs.

---
+++ backend/tcs-service/Repos/Interfaces/IUserRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tcs_service.Models;
+using tcs_service.Repos.Base;
+
+namespace tcs_service.Repos.Interfaces
+{
+    public interface IUserRepo : IRepo<User>
+    {
+        User Authenticate(string username, string password);
+    }
+}
---
+++ backend/tcs-service/Repos/UserRepo.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using tcs_service.Helpers;
+using tcs_service.Models;
+using tcs_service.Repos.Base;
+using tcs_service.Repos.Interfaces;
+
+namespace tcs_service.Repos
+{
+    public class UserRepo : BaseRepo<User>, IUserRepo
+    {
+
+        private readonly AppSettings _appSettings;
+
+
+
+        public UserRepo(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+
+        public User Authenticate(string username, string password)
+        {
+            var user = _db.Users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            // return null if user not found
+            if (user == null)
+                return null;
+
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            // remove password before returning
+            user.Password = null;
+
+            return user;
+        }
+
+        public override IEnumerable<User> GetAll()
+        {
+            // return users without passwords
+            return _db.Users.Where(x => string.IsNullOrEmpty(x.Password));
+        }
+    }
+}
