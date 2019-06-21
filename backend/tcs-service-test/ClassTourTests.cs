fe7d8c6608acfbf0b1b2f4574dca6e754098c327
seanrickard <srickard@wvup.edu>
2019-06-20 21:08:52 -0400

    Implemented ClassTour Tests

--- backend/tcs-service-test/ClassTourTests.cs
+++ backend/tcs-service-test/ClassTourTests.cs
@@ -1,16 +1,12 @@
 using AutoFixture;
 using AutoFixture.AutoMoq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections;
+using System.Net.Http;
+using System.Threading.Tasks;
 using tcs_service.Controllers;
-using tcs_service.EF;
 using tcs_service.Models;
-using tcs_service.Repos;
 using tcs_service.Repos.Interfaces;
 using Xunit;
 
@@ -18,59 +14,115 @@ namespace tcs_service_test
 {
     public class ClassTourTests
     {
-        [Fact]
-        public async void DisasterTest()
-        {
-            var dbContextMock = new Mock<TCSContext>();
-            var tourMock = new Mock<DbSet<ClassTour>>();
+        ClassTour classTour;
+        ClassToursController sut;
+        Mock<IClassTourRepo> repository;
+        IFixture fixture = new Fixture()
+           .Customize(new AutoMoqCustomization());
 
-            var fixture = new Fixture()
-            .Customize(new AutoMoqCustomization());
-
-            var tours = new List<ClassTour>();
-            var lockedUser = fixture.Build<ClassTour>().With(u => u.Name, "Harry").Create();
-            tours.Add(lockedUser);
-
-            var users = tours.AsQueryable();
-            var classTour = fixture.Create<ClassTour>();
-
-
-            tourMock.As<IQueryable<ClassTour>>().Setup(m => m.Provider).Returns(users.Provider);
-            tourMock.As<IQueryable<ClassTour>>().Setup(m => m.Expression).Returns(users.Expression);
-            tourMock.As<IQueryable<ClassTour>>().Setup(m => m.ElementType).Returns(users.ElementType);
-            tourMock.As<IQueryable<ClassTour>>().Setup(m => m.GetEnumerator()).Returns(users.GetEnumerator());
-
-            dbContextMock.Setup(x => x.ClassTours).Returns(tourMock.Object);
-
-            
-
-            var ClassTourRepo = new ClassTourRepo(dbContextMock.Object);
-
-            var harry = await ClassTourRepo.Find(lockedUser.ID);
-
-            Assert.Equal(harry.Name, lockedUser.Name);
-        }
-        [Fact]
-        public async void AddingNewTour()
-        {
-            var fixture = new Fixture()
-             .Customize(new AutoMoqCustomization());
-
-            var classTour = fixture.Create<ClassTour>();
-         
-            var repository = fixture.Create<Mock<IClassTourRepo>>();
-
-            await repository.Object.Add(classTour);
-           
-            
-            var tour = await repository.Object.Exist(classTour.ID);
-                       
-            
-            Assert.Equal(classTour.Name, classTour.Name );
-          
+        public ClassTourTests()
+        {       
+            classTour = fixture.Create<ClassTour>();
+            repository = new Mock<IClassTourRepo>();
+            sut = new ClassToursController(repository.Object);
         }
 
+        [Fact]
+        public async void GetTourWithID_ShouldWork_Returns200()
+        {
+            repository.Setup(x => x.Add(classTour)).ReturnsAsync(classTour);
 
-        
+            IActionResult results = await sut.GetClassTour(classTour.ID);
+            var okObjectResult = Assert.IsType<OkObjectResult>(results);
+            
+            Assert.Equal(classTour, okObjectResult.Value);
+            Assert.Equal(200, okObjectResult.StatusCode);   
+        }
+
+        [Fact]
+        public async void AddingNewTour_ShouldWork_Returns201()
+        {
+            repository.Setup(x => x.Find(classTour.ID)).ReturnsAsync(classTour);
+
+            IActionResult results = await sut.PostClassTour(classTour);
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(results);
+
+            Assert.Equal(classTour, createdAtActionResult.Value);
+            Assert.Equal(201, createdAtActionResult.StatusCode);
+        }
+
+        [Fact]
+        public async void UpdatingTour_ShouldWork_Returns200()
+        {
+            var updatedClassTour = fixture.Build<ClassTour>()
+                .With(x => x.ID, classTour.ID)
+                .With(x => x.Name, "updatedName")
+                .Create();
+
+            repository.Setup(x => x.Update(updatedClassTour)).ReturnsAsync(updatedClassTour);
+
+
+            IActionResult results = await sut.PutClassTour(classTour.ID, updatedClassTour);
+            var okObjectResult = Assert.IsType<OkObjectResult>(results);
+
+            Assert.Equal(updatedClassTour, okObjectResult.Value);
+            Assert.Equal(200, okObjectResult.StatusCode);
+        }
+
+        [Fact]
+        public async void UpdatingTourWithIDNotInDb_ShouldReturnBadRequest()
+        {
+            
+            IActionResult results = await sut.PutClassTour(classTour.ID + 1, classTour);
+            var badRequestResult = Assert.IsType<BadRequestResult>(results);
+
+            Assert.Equal(400, badRequestResult.StatusCode);
+        }
+
+
+        // This test fails. Need change in controller to make sure name cannot be set to null
+        [Fact]
+        public async void UpdatingTourWithNameSetToNull_ShouldReturnBadRequest()
+        {
+            var nullNameClassTour = fixture.Build<ClassTour>()
+                .With(x => x.ID, classTour.ID)
+                .Without(x => x.Name)
+                .Create();
+
+            IActionResult results = await sut.PutClassTour(classTour.ID, nullNameClassTour);
+            var badRequestResult = Assert.IsType<OkObjectResult>(results);
+
+            Assert.Equal(400, badRequestResult.StatusCode);
+        }
+
+        [Fact]
+        public async void DeleteTour_ShouldWork_Returns200()
+        {
+            repository.Setup(x => x.Exist(classTour.ID)).ReturnsAsync(true);
+            repository.Setup(x => x.Remove(classTour.ID)).ReturnsAsync(classTour);
+
+
+            IActionResult results = await sut.DeleteClassTour(classTour.ID);
+            var okObjectResult = Assert.IsType<OkObjectResult>(results);
+
+            Assert.Equal(classTour, okObjectResult.Value);
+            Assert.Equal(200, okObjectResult.StatusCode);
+        }
+
+        // Test fails because not mocking httpRequest yet
+        [Fact]
+        public async void GetAllTours_ShouldWork_Returns200andCount()
+        { 
+
+            var tours = fixture.CreateMany<ClassTour>();
+            repository.Setup(x => x.GetAll()).Returns(tours);
+            //Maybe need to make new httpClient
+            //sut.Request = HttpClient();
+            IActionResult results = sut.GetClassTour();
+            var objectResult = Assert.IsType<ObjectResult>(results);
+
+            Assert.Equal(tours, objectResult.Value);
+            Assert.Equal(200, objectResult.StatusCode);
+        }
     }
 }
