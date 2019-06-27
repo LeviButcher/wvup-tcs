3937bc50ea644c43378fde8135df282894c74795
Levi Butcher <lbutche3@wvup.edu>
2019-06-26 21:18:25 -0400

    Docker compose update for container networking

--- backend/tcs-service/Startup.cs
+++ backend/tcs-service/Startup.cs
@@ -1,24 +1,22 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using AutoMapper;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
 using tcs_service.EF;
-using tcs_service.Repos.Interfaces;
-using tcs_service.Repos;
 using tcs_service.Helpers;
+using tcs_service.Repos;
+using tcs_service.Repos.Interfaces;
 
-namespace tcs_service
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace tcs_service {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
@@ -27,72 +25,62 @@ namespace tcs_service
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(options =>
-            {
-                options.AddPolicy(AllowAnywhere,
-                  builder =>
-                  {
-                      builder.WithOrigins("*")
-                              .AllowAnyHeader()
-                              .AllowAnyMethod();
-                });
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddCors (options => {
+                options.AddPolicy (AllowAnywhere,
+                    builder => {
+                        builder.WithOrigins ("*")
+                            .AllowAnyHeader ()
+                            .AllowAnyMethod ();
+                    });
             });
 
-            services.AddAutoMapper(typeof(Startup));
-            var appSettingsSection = Configuration.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingsSection);
+            services.AddAutoMapper (typeof (Startup));
+            var appSettingsSection = Configuration.GetSection ("AppSettings");
+            services.Configure<AppSettings> (appSettingsSection);
 
             // configure jwt authentication
-            var appSettings = appSettingsSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
+            var appSettings = appSettingsSection.Get<AppSettings> ();
+            var key = Encoding.ASCII.GetBytes (appSettings.Secret);
+            services.AddAuthentication (x => {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer (x => {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey (key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
 
-            services.AddScoped<IClassTourRepo, ClassTourRepo>();
-            services.AddScoped<IUserRepo, UserRepo>();
+            services.AddScoped<IClassTourRepo, ClassTourRepo> ();
+            services.AddScoped<IUserRepo, UserRepo> ();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddDbContext<TCSContext>(options =>
-                    options.UseSqlServer(Configuration["DB:connectionString"]));
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
+            services.AddDbContext<TCSContext> (options =>
+                options.UseSqlServer (Configuration["DB:connectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, TCSContext db)
-        {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env, TCSContext db) {
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts ();
             }
-            app.UseCors(AllowAnywhere);
+            app.UseCors (AllowAnywhere);
 
-            DbInitializer.InitializeData(db);
+            DbInitializer.InitializeData (db);
 
-            app.UseAuthentication();
+            app.UseAuthentication ();
 
-            app.UseMvc();
+            app.UseMvc ();
         }
     }
 }
