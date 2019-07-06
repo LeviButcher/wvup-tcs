9b0ceb5ece5996835ea7f898e3d6f7d5712df97a
seanrickard <srickard@wvup.edu>
2019-07-06 19:42:16 -0400

    Get Tours By Date Added

--- backend/tcs-service/Controllers/ClassToursController.cs
+++ backend/tcs-service/Controllers/ClassToursController.cs
@@ -27,20 +27,7 @@ namespace tcs_service.Controllers
             return await _classTourRepo.Exist(id);
         }
 
-        [HttpGet]
-        [Produces(typeof(DbSet<ClassTour>))]
-        public IActionResult GetClassTour()
-        {
-            var results = new ObjectResult(_classTourRepo.GetAll())
-            {
-                StatusCode = (int)HttpStatusCode.OK
-            };
-
-            //Request.HttpContext.Response.Headers.Add("X-Total-Count", _classTourRepo.GetAll().Count().ToString());
-
-            return results;
-        }
-            
+             
         [HttpGet("{id}")]
         [Produces(typeof(ClassTour))]
         public async Task<IActionResult> GetClassTour([FromRoute] int id)
@@ -132,9 +119,9 @@ namespace tcs_service.Controllers
             return Ok(tour);
         }
 
-        public async Task<ActionResult<IEnumerable<ClassTour>>> GetBetweenDates(DateTime start, DateTime end)
-        {
-            return Ok(await _classTourRepo.GetBetweenDates(start, end));
-        }
+        //public async Task<ActionResult<IEnumerable<ClassTour>>> GetBetweenDates(DateTime start, DateTime end)
+        //{
+        //    return Ok(await _classTourRepo.GetBetweenDates(start, end));
+        //}
     }
 }
