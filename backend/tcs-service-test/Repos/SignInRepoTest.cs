b0987ed63476fbcbd08881a33434ca7cd3bf2cf2
Sean Rickard <E097861@wv.gov>
2019-07-08 12:48:53 -0400

    Update  Test to pass

--- backend/tcs-service-test/Repos/SignInRepoTest.cs
+++ backend/tcs-service-test/Repos/SignInRepoTest.cs
@@ -78,7 +78,6 @@ namespace tcs_service_test.Repos
 
             var course = fixture.Build<Course>()
                 .With(x => x.Department, dept)
-                .With(x => x.DepartmentID, dept.Code)
                 .Create();
 
             var res = await signInRepo.AddCourse(course);
