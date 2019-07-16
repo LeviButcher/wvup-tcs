1d245a5c74e776bf32bf373d87cf738a2a40e4aa
Sean Rickard <E097861@wv.gov>
2019-07-16 11:00:57 -0400

    remove tutoring from sample data

--- backend/tcs-service/SampleData/dbo.Reasons.data.sql
+++ backend/tcs-service/SampleData/dbo.Reasons.data.sql
@@ -2,5 +2,5 @@
 INSERT INTO [dbo].[Reasons] ([ID], [Name], [Deleted]) VALUES (1, N'Study Time', 1)
 INSERT INTO [dbo].[Reasons] ([ID], [Name], [Deleted]) VALUES (2, N'Computer Use', 1)
 INSERT INTO [dbo].[Reasons] ([ID], [Name], [Deleted]) VALUES (3, N'Printer Use', 0)
-INSERT INTO [dbo].[Reasons] ([ID], [Name], [Deleted]) VALUES (4, N'Tutoring', 1)
+INSERT INTO [dbo].[Reasons] ([ID], [Name], [Deleted]) VALUES (4, N'Bone Use', 1)
 SET IDENTITY_INSERT [dbo].[Reasons] OFF
