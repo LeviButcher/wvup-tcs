cbe7569d1131a2f24a5f67ba4d55272151ec0089
Levi Butcher <levibutcher355@gmail.com>
2019-08-04 21:56:08 -0400

    Added WeeklyVisits test

--- backend/tcs-service-test/Repos/ReportsRepoTest.cs
+++ backend/tcs-service-test/Repos/ReportsRepoTest.cs
@@ -3,11 +3,14 @@ using Xunit;
 using tcs_service.Repos;
 using tcs_service.EF;
 using tcs_service_test.Helpers;
+using System.Linq;
 using AutoFixture;
 using System.Collections.Generic;
 using tcs_service.Models.ViewModels;
 using tcs_service.Models;
 using AutoFixture.AutoMoq;
+using tcs_service.Services;
+using tcs_service.Services.Interfaces;
 
 namespace tcs_service_test.Repos
 {
@@ -18,14 +21,18 @@ namespace tcs_service_test.Repos
         IFixture fixture;
         TCSContext db;
 
+
         public ReportsRepoTest() {
             var dbOptions = DbInMemory.getDbInMemoryOptions(dbName);
             db = new TCSContext(dbOptions);
-            reportsRepo = new ReportsRepo(dbOptions);
+            IBannerService bs = new MockBannerService(dbOptions);
+            reportsRepo = new ReportsRepo(dbOptions, bs);
             fixture = new Fixture()
               .Customize(new AutoMoqCustomization());
             // Setting default values
-            fixture.Customize<SignIn>((ob) => ob.Without(x => x.Courses).Without(x => x.Person).Without(x => x.Semester));
+            fixture.RepeatCount = 3;
+            fixture.Customize<SignIn>((ob) => ob.Without(x => x.Courses).Without(x => x.Person)
+                .Without(x => x.Semester).Without(x => x.Reasons));
         }
 
         public void Dispose()
@@ -35,20 +42,67 @@ namespace tcs_service_test.Repos
 
         [Fact]
         public async void WeeklyVisits_HappyPath() {
-            var signins = fixture.Build<SignIn>().With(x => x.InTime, new DateTime()).With(x => x.OutTime, new DateTime()).CreateMany();
-            db.SignIns.AddRange(signins);
-
-            var startDate = new DateTime();
+            var startDate = new DateTime(2019, 8, 4);
             var endDate = startDate.AddDays(5);
+            var signins = fixture.CreateMany<SignIn>().Select(x => {
+                    x.InTime = startDate;
+                    x.OutTime = startDate.AddDays(2);
+                    return x;
+                });
+
+            db.SignIns.AddRange(signins);
+            db.SaveChanges();
+            
             var results = await reportsRepo.WeeklyVisits(startDate, endDate);
-            var expectedResult = new List<ReportCountViewModel>(){
-                new ReportCountViewModel{
-                    Item = 5,
+            var expectedResult = new List<WeeklyVisitsViewModel>(){
+                new WeeklyVisitsViewModel(startDate, startDate.AddDays(6)){
                     Count = 3
                 }
             };
             
-            Assert.Equal(results, expectedResult);
+            Assert.Equal(results[0].Count, expectedResult[0].Count);
+            Assert.Equal(results[0].Item, expectedResult[0].Item);
+        }
+
+        [Fact]
+        public async void WeeklyVisits_SignInsSpreadAcross3Weeks() {
+            var startingDate = new DateTime(2019, 8, 4);
+
+            var weeks = new []{
+                new {
+                    startDate = startingDate,
+                    endDate = startingDate.AddDays(6)
+                },
+                new {
+                    startDate = startingDate.AddDays(7),
+                    endDate = startingDate.AddDays(7 + 6)
+                },
+                new {
+                    startDate = startingDate.AddDays(14),
+                    endDate = startingDate.AddDays(14 + 6)
+                }
+            }.ToList();
+
+            var signIns = weeks.Aggregate(new List<SignIn>(), (acc, curr) => {
+                var weeksSignIns = fixture.CreateMany<SignIn>().Select(x => {
+                    x.InTime = curr.startDate;
+                    x.OutTime = curr.startDate.AddDays(1);
+                    return x;
+                });
+                acc.AddRange(weeksSignIns);
+                return acc;
+            });
+
+            db.SignIns.AddRange(signIns);
+            db.SaveChanges();
+            
+            var results = await reportsRepo.WeeklyVisits(weeks[0].startDate, weeks.Last().endDate);
+            var expectedResult = weeks.Select(x => new WeeklyVisitsViewModel(x.startDate, x.endDate)
+            {
+                Count = 3
+            }).ToList();
+            
+            Assert.Equal(expectedResult, results);
         }
     }
 }
--- backend/tcs-service/Models/ViewModels/WeeklyVisitsViewModel.cs
+++ backend/tcs-service/Models/ViewModels/WeeklyVisitsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Runtime.CompilerServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,8 +8,34 @@ namespace tcs_service.Models.ViewModels
 {
     public class WeeklyVisitsViewModel
     {
-        public string Item { get; set; }
+        public WeeklyVisitsViewModel(DateTime startDate, DateTime endDate) {
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public string Item => $"{startDate.Date.ToShortDateString()} - {endDate.ToShortDateString()}";
 
         public int Count { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            //
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+            
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            WeeklyVisitsViewModel wv = (WeeklyVisitsViewModel) obj;
+            // TODO: write your implementation of Equals() here
+            return startDate == wv.startDate && endDate == wv.endDate && Count == wv.Count;
+        }
     }
 }
