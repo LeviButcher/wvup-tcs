4fd6df4fa94470c3ddee2459b0d9c99558cdefc3
Levi Butcher <levibutcher355@gmail.com>
2019-09-02 13:36:03 -0400

    Seed default user

--- backend/tcs-service/EF/DbInitializer.cs
+++ backend/tcs-service/EF/DbInitializer.cs
@@ -1,64 +1,73 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
+using tcs_service.Models;
+using tcs_service.Repos.Interfaces;
 
 namespace tcs_service.EF
 {
     public class DbInitializer
     {
-        private TCSContext _context;
-
-        public DbInitializer(TCSContext context)
+        public async static Task InitializeData(TCSContext context, IUserRepo userRepo, IHostingEnvironment env)
         {
-            _context = context;
+            await context.Database.MigrateAsync();
+            if (env.IsDevelopment())
+            {
+                await ClearData(context);
+                await SeedData(context);
+            }
+            await SeedAdmin(context, userRepo);
         }
 
-        public static void InitializeData(TCSContext context)
+        private async static Task ClearData(TCSContext context)
         {
-            context.Database.Migrate();
-            ClearData(context);
-            SeedData(context);
+            await context.Database.ExecuteSqlCommandAsync("DELETE FROM [dbo].[ClassTours]");
+            await context.Database.ExecuteSqlCommandAsync("DELETE FROM [dbo].[SignInReasons]");
+            await context.Database.ExecuteSqlCommandAsync("DELETE FROM [dbo].[SignInCourses]");
+            await context.Database.ExecuteSqlCommandAsync("DELETE FROM [dbo].[SignIns]");
+            await context.Database.ExecuteSqlCommandAsync("DELETE FROM [dbo].[Courses]");
+            await context.Database.ExecuteSqlCommandAsync("DELETE FROM [dbo].[Departments]");
+            await context.Database.ExecuteSqlCommandAsync("DELETE FROM [dbo].[Reasons]");
+            await context.Database.ExecuteSqlCommandAsync("DELETE FROM [dbo].[People]");
+            await context.Database.ExecuteSqlCommandAsync("DELETE FROM [dbo].[Semesters]");
+            await context.Database.ExecuteSqlCommandAsync("DELETE FROM [dbo].[Users]");
         }
 
-        public static void ClearData(TCSContext context)
+        private async static Task SeedAdmin(TCSContext context, IUserRepo userRepo)
         {
-            context.Database.ExecuteSqlCommand("DELETE FROM [dbo].[ClassTours]");
-            context.Database.ExecuteSqlCommand("DELETE FROM [dbo].[SignInReasons]");
-            context.Database.ExecuteSqlCommand("DELETE FROM [dbo].[SignInCourses]");
-            context.Database.ExecuteSqlCommand("DELETE FROM [dbo].[SignIns]");
-            context.Database.ExecuteSqlCommand("DELETE FROM [dbo].[Courses]");
-            context.Database.ExecuteSqlCommand("DELETE FROM [dbo].[Departments]");
-            context.Database.ExecuteSqlCommand("DELETE FROM [dbo].[Reasons]");
-            context.Database.ExecuteSqlCommand("DELETE FROM [dbo].[People]");
-            context.Database.ExecuteSqlCommand("DELETE FROM [dbo].[Semesters]");
+            if (await context.Users.AnyAsync()) return;
+            var user = new User()
+            {
+                FirstName = "Change",
+                LastName = "Username",
+                Username = "tcs",
+            };
+            await userRepo.Create(user, "Develop@90");
         }
 
-        private static void SeedData(TCSContext context)
+        private async static Task SeedData(TCSContext context)
         {
-            string classTours = File.ReadAllText(@"./SampleData/dbo.ClassTours.data.sql");
-            string courses = File.ReadAllText(@"./SampleData/dbo.Courses.data.sql");
-            string departments = File.ReadAllText(@"./SampleData/dbo.Departments.data.sql");
-            string people = File.ReadAllText(@"./SampleData/dbo.People.data.sql");
-            string reasons = File.ReadAllText(@"./SampleData/dbo.Reasons.data.sql");
-            string semesters = File.ReadAllText(@"./SampleData/dbo.Semesters.data.sql");
-            string signIns = File.ReadAllText(@"./SampleData/dbo.SignIns.data.sql");
-            string signInReasons = File.ReadAllText(@"./SampleData/dbo.SignInReasons.data.sql");
-            string signInCourses = File.ReadAllText(@"./SampleData/dbo.SignInCourses.data.sql");
+            string classTours = await File.ReadAllTextAsync(@"./SampleData/dbo.ClassTours.data.sql");
+            string courses = await File.ReadAllTextAsync(@"./SampleData/dbo.Courses.data.sql");
+            string departments = await File.ReadAllTextAsync(@"./SampleData/dbo.Departments.data.sql");
+            string people = await File.ReadAllTextAsync(@"./SampleData/dbo.People.data.sql");
+            string reasons = await File.ReadAllTextAsync(@"./SampleData/dbo.Reasons.data.sql");
+            string semesters = await File.ReadAllTextAsync(@"./SampleData/dbo.Semesters.data.sql");
+            string signIns = await File.ReadAllTextAsync(@"./SampleData/dbo.SignIns.data.sql");
+            string signInReasons = await File.ReadAllTextAsync(@"./SampleData/dbo.SignInReasons.data.sql");
+            string signInCourses = await File.ReadAllTextAsync(@"./SampleData/dbo.SignInCourses.data.sql");
 
 
-            context.Database.ExecuteSqlCommand(classTours);
-            context.Database.ExecuteSqlCommand(departments);
-            context.Database.ExecuteSqlCommand(courses);
-            context.Database.ExecuteSqlCommand(people);
-            context.Database.ExecuteSqlCommand(reasons);
-            context.Database.ExecuteSqlCommand(semesters);
-            context.Database.ExecuteSqlCommand(signIns);
-            context.Database.ExecuteSqlCommand(signInReasons);
-            context.Database.ExecuteSqlCommand(signInCourses);
+            await context.Database.ExecuteSqlCommandAsync(classTours);
+            await context.Database.ExecuteSqlCommandAsync(departments);
+            await context.Database.ExecuteSqlCommandAsync(courses);
+            await context.Database.ExecuteSqlCommandAsync(people);
+            await context.Database.ExecuteSqlCommandAsync(reasons);
+            await context.Database.ExecuteSqlCommandAsync(semesters);
+            await context.Database.ExecuteSqlCommandAsync(signIns);
+            await context.Database.ExecuteSqlCommandAsync(signInReasons);
+            await context.Database.ExecuteSqlCommandAsync(signInCourses);
         }
-
     }
 }
