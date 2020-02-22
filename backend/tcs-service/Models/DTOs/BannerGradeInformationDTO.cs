88743dcb665d6f05a308b725af48f67a3148d858
Levi Butcher <levibutcher355@gmail.com>
2020-02-22 12:18:31 -0500

    Department field to BannerGradeInformation
    Banner Api was updated to return back the Department name when getting
    student grades. This allowed us to not have to change any of our reports
    because we had relied on that information being return in our mock of
    the api.

--- backend/tcs-service/Models/DTOs/BannerGradeInformationDTO.cs
+++ backend/tcs-service/Models/DTOs/BannerGradeInformationDTO.cs
@@ -7,5 +7,6 @@ namespace tcs_service.Models.DTO
         public string CourseNumber { get; set; }
         public string MidtermGrade { get; set; }
         public string FinalGrade { get; set; }
+        public string Department { get; set;}
     }
 }
